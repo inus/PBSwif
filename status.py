f2c3db38eeaa8dc16cd7abeea0c84a0a1b6256ae
Inus Scheepers <inus@bikalabs.com>
2023-06-17 19:22:00 +0200

    Display last 12 jobs essential status. To do: format in markdown

--- status.py
+++ status.py
@@ -1,22 +1,29 @@
-import re
+import re,json
 from subprocess import run
 
 def show_status(st, status_tab):
 
+  joblist = []
   with status_tab:
     jobs = run("qstat -xu $USER | awk '{ print $1 }' | tail -12 ",capture_output=True,shell=True)
-    for jobid  in jobs.stdout.splitlines():
-        jobdetail  = run("qstat -xf " + jobid.decode() + " | grep comment" ,capture_output=True,shell=True)
-        X = jobid.decode() + re.sub('comment = Job run at ', '', jobdetail.stdout.decode())
-        st.text(X)
+    jobids = [x for x in jobs.stdout.splitlines()]
+    jobids.reverse() #  in jobs.stdout.splitlines():
+    for jobid  in jobids:
+        jobdetail  = run("qstat -xf -F json " + jobid.decode(), capture_output=True, shell=True)
+        joblist.append(jobdetail.stdout)
 
-#        failed =  re.match('fail', X)
-#        if  re.search('fail',X):
-#                st.write(':red[' + X + ']')
-#        else:   
-#                st.write(':green[' + X + ']')
-#
+        X = jobid.decode() #+ re.sub('comment = Job run at ', '', jobdetail.stdout.decode())
 
-        
+        jobdict = json.loads(jobdetail.stdout)
+        st.write(str(jobid.decode()), '\t',
+                jobdict['Jobs'][X]['Job_Name'], '\t',
+                jobdict['Jobs'][X]['job_state'], '\t',
+                str(jobdict['Jobs'][X]['resources_used']['cpupercent']), '\t',
+                str(jobdict['Jobs'][X]['resources_used']['ncpus']), '\t',
+                str(jobdict['Jobs'][X]['resources_used']['cput']), '\t',
+                str(jobdict['Jobs'][X]['resources_used']['mem']), '\t',
+                str(jobdict['Jobs'][X]['Exit_status']), '\t',
+                (jobdict['Jobs'][X]['etime']),
+                )
 
 
